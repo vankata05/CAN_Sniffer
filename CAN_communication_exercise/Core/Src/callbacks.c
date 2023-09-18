be01056dbf8da58bb0ad025a42ab19f087716687
vankata05 <ivan.v.ivanov.2019@elsys-bg.org>
2023-09-18 10:26:30 +0300

    Send request only after recieving responce from the previous request in order to not flood the can

--- CAN_communication_exercise/Core/Src/callbacks.c
+++ CAN_communication_exercise/Core/Src/callbacks.c
@@ -10,7 +10,7 @@
 
 #include "flags.h"
 
-extern uint8_t* getState();
+//extern uint8_t* getState();
 
 uint8_t data[8];
 char data1[64];
@@ -18,13 +18,14 @@ char data1[64];
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
 {
 //	if(BDTKTD == 1){
-
-	  CAN_RxHeaderTypeDef rxPHead = {0};
-	  HAL_CAN_GetRxMessage(&hcan1, CAN_RX_FIFO0, &rxPHead, data);
-	  sprintf(data1, "0x%X, 0x%X, 0x%X, 0x%X, 0x%X, 0x%X, 0x%X, 0x%X, 0x%X\n", (unsigned int)rxPHead.StdId, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]);
+  CAN_RxHeaderTypeDef rxPHead = {0};
+  HAL_CAN_GetRxMessage(&hcan1, CAN_RX_FIFO0, &rxPHead, data);
+  if(rxPHead.StdId == 0x7E8)
+	  IRQTX = 1;
+  sprintf(data1, "0x%X, 0x%X, 0x%X, 0x%X, 0x%X, 0x%X, 0x%X, 0x%X, 0x%X\n", (unsigned int)rxPHead.StdId, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]);
 //	  HAL_CAN_GetRxMessage(&hcan2, CAN_RX_FIFO0, &rxPHead, data);
 //	  if(rxPHead.StdId == 0x7E8){
-	  CDC_Transmit_FS((uint8_t*)data1, strlen(data1));
+  CDC_Transmit_FS((uint8_t*)data1, strlen(data1));
 //	  }
 //	  CDC_Transmit_FS(data, 8);
 //	  CDC_Transmit_FS((uint8_t)rxPHead.StdId, 2);
--- CAN_communication_exercise/Core/Src/main.c
+++ CAN_communication_exercise/Core/Src/main.c
@@ -28,9 +28,10 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-volatile uint8_t IRQRX0;
-volatile uint8_t IRQRX1;
-volatile uint8_t IRQTX;
+volatile uint8_t IRQRX0 = 0;
+volatile uint8_t IRQRX1 = 0;
+volatile uint8_t IRQTX = 0;
+volatile uint8_t BDTKTD = 0;
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
@@ -137,7 +138,10 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  Capture_PID_Snapshot();
+	  if(IRQTX == 1){
+		  Capture_PID_Snapshot();
+		  IRQTX = 0;
+	  }
   }
   /* USER CODE END 3 */
 }
@@ -151,35 +155,32 @@ static void HODL_Till_BTN(void){
 
 static void Auto_Baudrate_Setup(uint32_t PRE[]){
   uint8_t i;
-  for(i = 0; i < 5; i++){
+  for(i = 0; i < 4; i++){
 	  if(IRQRX1 == 0 && IRQRX0 == 0){
 		  if(i == 0){
 			  MX_CAN1_Init(PRE[0], CAN_MODE_SILENT);
 			  HAL_CAN_Start(&hcan1);
 			  HAL_Delay(100);
 		  }else{
-//			  HAL_GPIO_LockPin(GPIOD, GPIO_PIN_0);
-//			  HAL_GPIO_LockPin(GPIOD, GPIO_PIN_1);
 			  HAL_CAN_Stop(&hcan1);
-//			  HAL_Delay(100);
 			  MX_CAN1_Init(PRE[i], CAN_MODE_SILENT);
 			  HAL_CAN_Start(&hcan1);
 			  HAL_Delay(100);
 		  }
 	  }else{
-		  if(IRQRX1 == 1 || IRQRX0 == 1){
-			  char str[42];
-			  sprintf(str, "BAUDRATE DETECTED WITH PRESCALER OF %ld\n", PRE[i]);
-			  CDC_Transmit_FS((uint8_t*)str, strlen(str));
-			  HAL_CAN_Stop(&hcan1);
-//			  MX_CAN1_Init(PRE[i], CAN_MODE_NORMAL);
-			  hcan1.Init.Mode = CAN_MODE_NORMAL;
-			  HAL_CAN_Init(&hcan1);
-//			  CAN1_Filter_Config();
-			  HAL_CAN_Start(&hcan1);
-			  break;
-		  }
+		  break;
 	  }
+  }
+
+  if(IRQRX1 >= 1 || IRQRX0 >= 1){
+	  char str[42];
+	  sprintf(str, "BAUDRATE DETECTED WITH PRESCALER OF %ld\n", PRE[i-1]);
+	  CDC_Transmit_FS((uint8_t*)str, strlen(str));
+	  HAL_CAN_Stop(&hcan1);
+	  hcan1.Init.Mode = CAN_MODE_NORMAL;
+	  HAL_CAN_Init(&hcan1);
+	  CAN1_Filter_Config();
+	  HAL_CAN_Start(&hcan1);
   }
 
   if(IRQRX1 == 0 && IRQRX0 == 0){
@@ -200,83 +201,11 @@ static void Capture_PID_Snapshot(void){
 
 	  uint8_t data[] = {0x02, 0x01, 0x00, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA};
 
-	  CAN_TxHeaderTypeDef TPpHead;
-	  TPpHead.StdId = 0x700;
-	  TPpHead.IDE = CAN_ID_STD;
-	  TPpHead.RTR = CAN_RTR_DATA;
-	  TPpHead.DLC = 8;
-
-	  HAL_Delay(10);
-
-	  uint8_t TP_data[] = {0x02, 0x3E, 0x00, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA};
-	//  uint8_t readVin_Req[] = {0x04, 0x22, 0xF1, 0x90, 0x00, 0x00, 0x00, 0x00};
-
-	  HAL_Delay(10);
-
-	//  **Tester Present**
-	  HAL_CAN_AddTxMessage(&hcan1, &TPpHead, TP_data, &mailbox);
+	  HAL_Delay(100);
 
 	  //  **PIDs supported(01-20)**
 	  HAL_CAN_AddTxMessage(&hcan1, &pHead, data, &mailbox);
 
-	  HAL_Delay(10);
-
-	  //  **Tester Present**
-	    HAL_CAN_AddTxMessage(&hcan1, &TPpHead, TP_data, &mailbox);
-
-	  //  **PIDs supported(21-40)**
-	  data[2] = 0x20;
-	  HAL_CAN_AddTxMessage(&hcan1, &pHead, data, &mailbox);
-
-	  HAL_Delay(10);
-
-	  //  **Tester Present**
-	    HAL_CAN_AddTxMessage(&hcan1, &TPpHead, TP_data, &mailbox);
-
-	  //  **PIDs supported(41-60)**
-	  data[2] = 0x40;
-	  HAL_CAN_AddTxMessage(&hcan1, &pHead, data, &mailbox);
-
-	  HAL_Delay(10);
-
-	  //  **Tester Present**
-	    HAL_CAN_AddTxMessage(&hcan1, &TPpHead, TP_data, &mailbox);
-
-	  //  **PIDs supported(61-80)**
-	  data[2] = 0x60;
-	  HAL_CAN_AddTxMessage(&hcan1, &pHead, data, &mailbox);
-
-	  HAL_Delay(10);
-
-	  //  **Tester Present**
-	    HAL_CAN_AddTxMessage(&hcan1, &TPpHead, TP_data, &mailbox);
-
-	  //  **PIDs supported(81-A0)**
-	  data[2] = 0x80;
-	  HAL_CAN_AddTxMessage(&hcan1, &pHead, data, &mailbox);
-
-	  HAL_Delay(10);
-
-	  //  **Tester Present**
-	    HAL_CAN_AddTxMessage(&hcan1, &TPpHead, TP_data, &mailbox);
-
-	  //  **PIDs supported(A1-C0)**
-	  data[2] = 0xA0;
-	  HAL_CAN_AddTxMessage(&hcan1, &pHead, data, &mailbox);
-
-	  HAL_Delay(10);
-
-	  //  **Tester Present**
-	    HAL_CAN_AddTxMessage(&hcan1, &TPpHead, TP_data, &mailbox);
-
-	  //  **PIDs supported(C1-E0)**
-	  data[2] = 0xC0;
-	  HAL_CAN_AddTxMessage(&hcan1, &pHead, data, &mailbox);
-
-	  HAL_Delay(10);
-
-	  //  **Tester Present**
-	    HAL_CAN_AddTxMessage(&hcan1, &TPpHead, TP_data, &mailbox);
 }
 
 /**
